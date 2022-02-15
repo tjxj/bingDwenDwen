9d8949f2856fc15b8006a80e56935daafac6ff50
tjxj <1030999281@qq.com>
2022-02-15 10:48:15 +0800

    1.0

--- fourier_functins/fourier.py
+++ fourier_functins/fourier.py
@@ -12,7 +12,7 @@ from manimlib import *
 ########################################################################################
 
 
-class FourierCirclesScene(ZoomedScene):
+class FourierCirclesScene(Scene):
     CONFIG = {
         "n_vectors": 10,
         "big_radius": 2,
@@ -61,7 +61,7 @@ class FourierCirclesScene(ZoomedScene):
     }
 
     def setup(self):
-        ZoomedScene.setup(self)
+        Scene.setup(self)
         self.slow_factor_tracker = ValueTracker(
             self.slow_factor
         )
